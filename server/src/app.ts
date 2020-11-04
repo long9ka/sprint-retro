3a5b8e228c26e9a2109b3c4b0e07c7d2c988307a
long <i@x.localdomain>
2020-11-04 15:01:50 +0700

    init server

---
+++ server/src/app.ts
@@ -0,0 +1,10 @@
+import express, { Application, Request, Response } from "express";
+import { PORT } from "./config";
+
+const app: Application = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World");
+});
+ 
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
